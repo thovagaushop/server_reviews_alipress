298093c32c2e096c908528e6c7e2a81a639827b9
thovagaushop <minhmeomeomun@gmail.com>
2022-10-12 16:14:21 +0700

    upcode l2

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const axios = require('axios')
+const dotenv = require('dotenv')
 const bodyParser = require('body-parser')
 var cors = require('cors')
 const express = require('express')
+
+dotenv.config()
+
 const app = express()
 const port = 3000
 
@@ -12,7 +16,7 @@ app.post('/reviews',async (req, res, next) => {
     try {
         // const data = await axios.get('')
         console.log(req.body);
-        const data = await axios.get(`https://app.reviewapi.io/api/v1/reviews?apikey=4ec224c0-4994-11ed-85e0-2d7a967e64d2&url=${req.body.productURL}&amount=${req.body.amount}`)
+        const data = await axios.get(`https://app.reviewapi.io/api/v1/reviews?apikey=${process.env.API_KEY}&url=${req.body.productURL}&amount=${req.body.amount}`)
         res.json(data.data)
     } 
     catch(err) {
